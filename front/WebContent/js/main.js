e544527c41376e8eca6ebb5fc4d3db43f769a3b5
leketo <leketo03@gmail.com>
2018-10-22 22:27:03 -0300

    first commit

---
+++ front/WebContent/js/main.js
@@ -0,0 +1,32 @@
+table_espanol = {
+		  //"sProcessing": "Procesando...",
+		  "sProcessing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Cargando...</span>',
+		  "sLengthMenu": "Mostrar _MENU_ registros",
+		  "sZeroRecords": "No se encontraron resultados",
+		  "sEmptyTable": "Ningún dato disponible en esta tabla",
+		  "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+		  "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+		  "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+		  "sInfoPostFix": "",
+		  "sSearch": "Buscar:",
+		  "sUrl": "",
+		  "sInfoThousands": ",",
+		  "sLoadingRecords": "Cargando...",
+		  "decimal": ",",
+		  "thousands": ".",
+		  "oPaginate": {
+		    "sFirst": "Primero",
+		    "sLast": "Último",
+		    "sNext": "Siguiente",
+		    "sPrevious": "Anterior"
+		  },
+		  "oAria": {
+		    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+		    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+		  }
+		};
+
+row_buttons = '<span class="custom-checkbox"> <input type="checkbox" id="checkbox1" name="options[]" value="1"> <label for="checkbox1"></label> </span>'
+//row_buttons = '<button type="button" id="editar" class="btn btn-default btn-sm" data-toggle="tooltip" data-placement="top" title="Editar Fila"><span class="fa fa-pencil"></span></button><button type="button" id="consultar_eliminar" class="btn btn-default btn-sm" data-toggle="tooltip" data-placement="top" title="Eliminar Fila"><span class="fa fa-trash"></span></button>';
+
+row_buttons_details = '<button type="button" id="editar_detalle" class="btn btn-default btn-sm" data-placement="top" title="Editar Detalle"><span class="fa fa-pencil"></span></button><button type="button" id="eliminar_detalle" class="btn btn-default btn-sm" data-toggle="tooltip" data-placement="top" title="Eliminar Detalle"><span class="fa fa-trash"></span></button>';
